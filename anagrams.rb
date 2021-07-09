dc21fa3d237d624330f6b2485452d1ce440948a8
Richard Leung <61563481+Richardzleung@users.noreply.github.com>
2021-07-09 12:42:02 -0700

    richard driver

--- anagrams.rb
+++ anagrams.rb
@@ -12,11 +12,20 @@ end
 
 
 def second_anagram?(str1, str2)
-    
+    str1 = str1.chars
+    str2 = str2.chars
+    str1.each do |char_1|
+        if str2.index(char_1)
+            str2.delete_at(str2.index(char_1))
+        else
+            return false
+        end
+    end
+    str2.length.zero?
 end
 
 
 
-# p anagram?("gizmo", "sally")    #=> false
-# p anagram?("elvis", "lives")    #=> true
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
 
